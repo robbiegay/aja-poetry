2d575e60e2a8d9bd302f03f3cfba79d7aa34dd61
Robbie Gay <robbiecgay@gmail.com>
2020-04-11 23:15:45 -0400

    Updates

---
+++ precache-manifest.5e367394960273c68d9a5d738da113bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032bb4b0e0ae3ea12bd9580167e62615",
+    "url": "/robbiegay/aja-poetry/index.html"
+  },
+  {
+    "revision": "f3298f4ab0b19551356d",
+    "url": "/robbiegay/aja-poetry/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "6d5c097c2590694ad20d",
+    "url": "/robbiegay/aja-poetry/static/css/main.deb1f264.chunk.css"
+  },
+  {
+    "revision": "f3298f4ab0b19551356d",
+    "url": "/robbiegay/aja-poetry/static/js/2.20d32ef3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/robbiegay/aja-poetry/static/js/2.20d32ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5c097c2590694ad20d",
+    "url": "/robbiegay/aja-poetry/static/js/main.220f84a4.chunk.js"
+  },
+  {
+    "revision": "b8c3634ea1d060a7590f",
+    "url": "/robbiegay/aja-poetry/static/js/runtime-main.15f0bf49.js"
+  }
+]);
