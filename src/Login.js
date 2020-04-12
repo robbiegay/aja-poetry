8c6258d97132df6dcfe50a9f60d3782e05f8c8d9
Robbie Gay <robbiecgay@gmail.com>
2020-04-12 01:32:47 -0400

    Disable login form button

--- src/Login.js
+++ src/Login.js
@@ -18,12 +18,14 @@ function Login() {
                             Only Aja can login here.
                         </Form.Text>
                     </Form.Group>
-
                     <Form.Group controlId="password">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
+                        <Form.Text className="text-muted">
+                            [This feature is currently under construction -Robbie]
+                        </Form.Text>
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" type="submit" disabled>
                         Login
                     </Button>
                 </Form>
